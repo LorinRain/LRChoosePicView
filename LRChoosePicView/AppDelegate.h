3e34b5486e4037e8bdbb0bd60a8f32e8a529c216
仁心医生 <renxin@renxinyishengdeimac.local>
2016-06-23 16:16:02 +0800

    Initial Commit

---
+++ LRChoosePicView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRChoosePicView
+//
+//  Created by 仁心医生 on 16/6/23.
+//  Copyright © 2016年 LorinRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LRChoosePicView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LRChoosePicView
+//
+//  Created by 仁心医生 on 16/6/23.
+//  Copyright © 2016年 LorinRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
